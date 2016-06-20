4a7e4432e9f990c00f7aae0dd9173b12a0ee2420
Roman <roman.pawliw@gmail.com>
2016-06-20 19:27:46 +0300

    Add files via upload

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,24 @@
 (function() {
-var socket = io("http://localhost:3000/");
-//var socket = io("https://blooming-lake-49901.herokuapp.com/");
-var canvas = document.getElementById("myCanvas");
-var counter = 0;
+//var socket = io("http://localhost:3000/");
+var socket = io("https://blooming-lake-49901.herokuapp.com/"),
+    canvas = document.getElementById("myCanvas"),
+    counter = 0;
 
 ctx = canvas.getContext("2d");
 ctx.strokeStyle = "red";
 ctx.lineWidth = 10;
 
-function drawNet(x1, y1, x2, y2, x3, y3, x4, y4) {
+function drawCross(x1, y1, x2, y2, x3, y3, x4, y4) {
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
   ctx.stroke();
+
+  ctx.moveTo(x3, y3);
+  ctx.lineTo(x4, y4);
+  ctx.stroke();
 }
 
-function draw(x1, y1, x2, y2, x3, y3, x4, y4) {
+function draw(x1, y1, x2, y2, x3, y3, x4, y4, x, y) {
   switch (counter % 2) {
     case 0:
       ctx.moveTo(x1, y1);
@@ -25,41 +29,43 @@ function draw(x1, y1, x2, y2, x3, y3, x4, y4) {
       ctx.lineTo(x4, y4);
       ctx.stroke();
       break;
-    case 1:
+    case 1: 
       ctx.beginPath();
-      ctx.arc(x1, y1, 50, 0, 2 * Math.PI);
+      ctx.arc(x, y, 50, 0, 2 * Math.PI);
       ctx.stroke();
   }
   counter++;
 }
     
-drawNet(160, 0, 160, 500);
-drawNet(330, 0, 330, 500);
-drawNet(0, 160, 500, 160);
-drawNet(0, 330, 500, 330);
-
-canvas.onmousemove = function(e) {
-  console.log(e.offsetY, e.offsetX);
-}
+drawCross(160, 0, 160, 500, 330, 0, 330, 500);
+drawCross(0, 160, 500, 160, 0, 330, 500, 330);
 
 canvas.onclick = function(e) {
   if (e.offsetY < 160 && e.offsetX < 160) {
-    draw(25, 25, 135, 135, 135, 25, 25, 135);
-  } else if (e.offsetY < 160 && e.offsetX > 160 && e.offsetX < 330) {
-    draw(250, 80);
-  } else if (e.offsetY < 160 && e.offsetX > 330) {
+    draw(30, 30, 130, 130, 130, 30, 30, 130, 80, 80);
+  } 
+  else if (e.offsetY < 160 && e.offsetX > 160 && e.offsetX < 330) {
+  
+  } 
+  else if (e.offsetY < 160 && e.offsetX > 330) {
     console.log("pressed 3");
-  } else if (e.offsetY > 160 && e.offsetY < 330 && e.offsetX < 160) {
+  } 
+  else if (e.offsetY > 160 && e.offsetY < 330 && e.offsetX < 160) {
     console.log("pressed 4");
-  } else if (e.offsetY > 160 && e.offsetY < 330 && e.offsetX > 160 && e.offsetX < 330) {
+  } 
+  else if (e.offsetY > 160 && e.offsetY < 330 && e.offsetX > 160 && e.offsetX < 330) {
     console.log("pressed 5");
-  } else if (e.offsetY > 160 && e.offsetY < 330 && e.offsetX > 330) {
+  } 
+  else if (e.offsetY > 160 && e.offsetY < 330 && e.offsetX > 330) {
     console.log("pressed 6");
-  } else if (e.offsetY > 330 && e.offsetX < 160) {
+  } 
+  else if (e.offsetY > 330 && e.offsetX < 160) {
     console.log("pressed 7");
-  } else if (e.offsetY > 330 && e.offsetX > 160 && e.offsetX < 330) {
+  } 
+  else if (e.offsetY > 330 && e.offsetX > 160 && e.offsetX < 330) {
     console.log("pressed 8");
-  } else if (e.offsetY > 330 && e.offsetX > 330) {
+  } 
+  else if (e.offsetY > 330 && e.offsetX > 330) {
     console.log("pressed 9");
   }
 }
