afb3b1e9eba6f7659589dfdb8546a153725963f6
Roman <roman.pawliw@gmail.com>
2016-06-20 21:20:06 +0300

    Add files via upload

--- public/js/app.js
+++ public/js/app.js
@@ -8,13 +8,9 @@ ctx = canvas.getContext("2d");
 ctx.strokeStyle = "red";
 ctx.lineWidth = 10;
 
-function drawCross(x1, y1, x2, y2, x3, y3, x4, y4) {
+function drawCross(x1, y1, x2, y2) {
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
-  ctx.stroke();
-
-  ctx.moveTo(x3, y3);
-  ctx.lineTo(x4, y4);
   ctx.stroke();
 }
 
@@ -37,18 +33,20 @@ function draw(x1, y1, x2, y2, x3, y3, x4, y4, x, y) {
   counter++;
 }
     
-drawCross(160, 0, 160, 500, 330, 0, 330, 500);
-drawCross(0, 160, 500, 160, 0, 330, 500, 330);
+drawCross(160, 0, 160, 500);
+drawCross(330, 0, 330, 500);
+drawCross(0, 160, 500, 160);
+drawCross(0, 330, 500, 330);
 
 canvas.onclick = function(e) {
   if (e.offsetY < 160 && e.offsetX < 160) {
     draw(30, 30, 130, 130, 130, 30, 30, 130, 80, 80);
   } 
   else if (e.offsetY < 160 && e.offsetX > 160 && e.offsetX < 330) {
-  
+    draw(196, 30, 296, 130, 296, 30, 196, 130, 246, 80);
   } 
   else if (e.offsetY < 160 && e.offsetX > 330) {
-    console.log("pressed 3");
+    draw(362, 30, 462, 130, 462, 30, 362, 130, 412, 80);
   } 
   else if (e.offsetY > 160 && e.offsetY < 330 && e.offsetX < 160) {
     console.log("pressed 4");
