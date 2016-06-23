e9f670cb84e98bccc5e1fd2275ea1ba28deb6f27
Roman <roman.pawliw@gmail.com>
2016-06-23 21:55:40 +0300

    Add files via upload

--- public/js/app.js
+++ public/js/app.js
@@ -9,62 +9,64 @@ ctx = canvas.getContext("2d");
 ctx.strokeStyle = "red";
 ctx.lineWidth = 10;
 
-function drawX(x1, y1, x2, y2, x3, y3, x4, y4) {
-  ctx.moveTo(x1, y1);
-  ctx.lineTo(x2, y2);
-  ctx.stroke();
+var draw = {
+  drawX: function(x1, y1, x2, y2, x3, y3, x4, y4) {
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
 
-  ctx.moveTo(x3, y3);
-  ctx.lineTo(x4, y4);
-  ctx.stroke();
-}
+    ctx.moveTo(x3, y3);
+    ctx.lineTo(x4, y4);
+    ctx.stroke();
+  },
 
-function drawZero(x, y) {
-  ctx.beginPath();
-  ctx.arc(x, y, 50, 0, 2 * Math.PI);
-  ctx.stroke();
-}
+  drawZero: function(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, 50, 0, 2 * Math.PI);
+    ctx.stroke();
+  },
 
-function draw(x1, y1, x2, y2, x3, y3, x4, y4, x, y, pos) {
-  switch (counter % 2) {
-    case 0:
-      drawX(x1, y1, x2, y2, x3, y3, x4, y4);
-      break;
-    case 1: 
-      drawZero(x, y);
+  drawSymbol: function (x1, y1, x2, y2, x3, y3, x4, y4, x, y, pos) {
+    switch (counter % 2) {
+      case 0:
+        this.drawX(x1, y1, x2, y2, x3, y3, x4, y4, pos);
+        break;
+      case 1:
+      this.drawZero(x, y);
+    }
   }
 }
-    
-drawX(160, 0, 160, 500, 330, 0, 330, 500);
-drawX(0, 160, 500, 160, 0, 330, 500, 330);
+  
+draw.drawX(160, 0, 160, 500, 330, 0, 330, 500);
+draw.drawX(0, 160, 500, 160, 0, 330, 500, 330);
 
 canvas.onclick = function(e) {
   if (e.offsetY < 160 && e.offsetX < 160) {
-    draw(30, 30, 130, 130, 130, 30, 30, 130, 80, 80, 0);
+    draw.drawSymbol(30, 30, 130, 130, 130, 30, 30, 130, 80, 80, 0);
   } 
   else if (e.offsetY < 160 && e.offsetX > 160 && e.offsetX < 330) {
-    draw(196, 30, 296, 130, 296, 30, 196, 130, 246, 80, 1);
+    draw.drawSymbol(196, 30, 296, 130, 296, 30, 196, 130, 246, 80, 1);
   } 
   else if (e.offsetY < 160 && e.offsetX > 330) {
-    draw(362, 30, 462, 130, 462, 30, 362, 130, 412, 80, 2);
+    draw.drawSymbol(362, 30, 462, 130, 462, 30, 362, 130, 412, 80, 2);
   } 
   else if (e.offsetY > 160 && e.offsetY < 330 && e.offsetX < 160) {
-    draw(30, 196, 130, 296, 130, 196, 30, 296, 80, 246, 3);
+    draw.drawSymbol(30, 196, 130, 296, 130, 196, 30, 296, 80, 246, 3);
   } 
   else if (e.offsetY > 160 && e.offsetY < 330 && e.offsetX > 160 && e.offsetX < 330) {
-    draw(196, 196, 296, 296, 296, 196, 196, 296, 246, 246, 4);
+    draw.drawSymbol(196, 196, 296, 296, 296, 196, 196, 296, 246, 246, 4);
   } 
   else if (e.offsetY > 160 && e.offsetY < 330 && e.offsetX > 330) {
-    draw(362, 196, 462, 296, 462, 196, 362, 296, 412, 246, 5);
+    draw.drawSymbol(362, 196, 462, 296, 462, 196, 362, 296, 412, 246, 5);
   } 
   else if (e.offsetY > 330 && e.offsetX < 160) {
-    draw(30, 362, 130, 462, 130, 362, 30, 462, 80, 412, 6);
+    draw.drawSymbol(30, 362, 130, 462, 130, 362, 30, 462, 80, 412, 6);
   } 
   else if (e.offsetY > 330 && e.offsetX > 160 && e.offsetX < 330) {
-    draw(196, 362, 296, 462, 296, 362, 196, 462, 246, 412, 7);
+    draw.drawSymbol(196, 362, 296, 462, 296, 362, 196, 462, 246, 412, 7);
   } 
   else if (e.offsetY > 330 && e.offsetX > 330) {
-    draw(362, 362, 462, 462, 462, 362, 362, 462, 412, 412, 8);
+    draw.drawSymbol(362, 362, 462, 462, 462, 362, 362, 462, 412, 412, 8);
   }
   counter++;
 }
