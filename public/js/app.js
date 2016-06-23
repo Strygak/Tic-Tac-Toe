95593c7b20a868d645de60c9272337cdbe8f3b63
Roman <roman.pawliw@gmail.com>
2016-06-23 20:58:33 +0300

    Add files via upload

--- public/js/app.js
+++ public/js/app.js
@@ -9,39 +9,34 @@ ctx = canvas.getContext("2d");
 ctx.strokeStyle = "red";
 ctx.lineWidth = 10;
 
-function drawCross(x1, y1, x2, y2) {
+function drawX(x1, y1, x2, y2, x3, y3, x4, y4) {
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
+  ctx.stroke();
+
+  ctx.moveTo(x3, y3);
+  ctx.lineTo(x4, y4);
+  ctx.stroke();
+}
+
+function drawZero(x, y) {
+  ctx.beginPath();
+  ctx.arc(x, y, 50, 0, 2 * Math.PI);
   ctx.stroke();
 }
 
 function draw(x1, y1, x2, y2, x3, y3, x4, y4, x, y, pos) {
-  if (!squares[pos]) {
-    switch (counter % 2) {
-      case 0:
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x2, y2);
-        ctx.stroke();
-
-        ctx.moveTo(x3, y3);
-        ctx.lineTo(x4, y4);
-        ctx.stroke();
-        squares[pos] = "cross";
-        break;
-      case 1: 
-        ctx.beginPath();
-        ctx.arc(x, y, 50, 0, 2 * Math.PI);
-        ctx.stroke();
-        squares[pos] = "zero";
-    }
-    counter++;
+  switch (counter % 2) {
+    case 0:
+      drawX(x1, y1, x2, y2, x3, y3, x4, y4);
+      break;
+    case 1: 
+      drawZero(x, y);
   }
 }
     
-drawCross(160, 0, 160, 500);
-drawCross(330, 0, 330, 500);
-drawCross(0, 160, 500, 160);
-drawCross(0, 330, 500, 330);
+drawX(160, 0, 160, 500, 330, 0, 330, 500);
+drawX(0, 160, 500, 160, 0, 330, 500, 330);
 
 canvas.onclick = function(e) {
   if (e.offsetY < 160 && e.offsetX < 160) {
@@ -71,5 +66,6 @@ canvas.onclick = function(e) {
   else if (e.offsetY > 330 && e.offsetX > 330) {
     draw(362, 362, 462, 462, 462, 362, 362, 462, 412, 412, 8);
   }
+  counter++;
 }
 }())
