6a8ebea935c94baf3de8ccbd0e2ee37a04f75264
Roman <roman.pawliw@gmail.com>
2016-07-26 17:22:30 +0300

    Add files via upload

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,9 @@
-(function() {
-var socket = io("http://localhost:3000/");
-//var socket = io("https://blooming-lake-49901.herokuapp.com/"),
+//var socket = io("http://localhost:3000/");
+var socket = io("https://blooming-lake-49901.herokuapp.com/"),
     canvas = document.getElementById("myCanvas"),
     counter = 0,
-    squares = [];
+    squares = [],
+    sq0 = sq1 = sq2 = sq3 = sq4 = sq5 = sq6 = sq7 = sq8 = false;
 
 ctx = canvas.getContext("2d");
 ctx.strokeStyle = "red";
@@ -20,7 +20,7 @@ var draw = {
     ctx.stroke();
   },
 
-  drawZero: function(x, y) {
+  drawZero: function(x, y, pos) {
     ctx.beginPath();
     ctx.arc(x, y, 50, 0, 2 * Math.PI);
     ctx.stroke();
@@ -32,7 +32,7 @@ var draw = {
         this.drawX(x1, y1, x2, y2, x3, y3, x4, y4, pos);
         break;
       case 1:
-        this.drawZero(x, y);
+        this.drawZero(x, y, pos);
     }
     socket.emit("draw", {
       s1: x1,
@@ -60,6 +60,42 @@ var draw = {
     }
   },
 }
+
+function checkSquares() {
+    if (sq0 && sq1 && sq2) { 
+        draw.drawX(15, 80, 475, 80);
+        socket.emit("line", { x1: 15, y1: 80, x2: 475, y2: 80 }); 
+    }
+    else if (sq3 && sq4 && sq5) { 
+        draw.drawX(15, 245, 475, 245);
+        socket.emit("line", { x1: 15, y1: 245, x2: 475, y2: 245 }); 
+    }
+    else if (sq6 && sq7 && sq8) { 
+        draw.drawX(15, 410, 475, 410);
+        socket.emit("line", { x1: 15, y1: 410, x2: 475, y2: 410 }); 
+    }
+    else if (sq0 && sq3 && sq6) { 
+        draw.drawX(80, 15, 80, 475);
+        socket.emit("line", { x1: 80, y1: 15, x2: 80, y2: 475}); 
+    }
+    else if (sq1 && sq4 && sq7) { 
+        draw.drawX(245, 15, 245, 475);
+        socket.emit("line", { x1: 245, y1: 15, x2: 245, y2: 475 }); 
+    }
+    else if (sq2 && sq5 && sq8) { 
+        draw.drawX(412, 15, 412, 475);
+        socket.emit("line", { x1: 412, y1: 15, x2: 412, y2: 475 }); 
+    }
+    else if (sq0 && sq4 && sq8) { 
+        draw.drawX(20, 20, 470, 470);
+        socket.emit("line", { x1: 20, y1: 20, x2: 470, y2: 470 }); 
+    }
+    else if (sq2 && sq4 && sq6) { 
+        draw.drawX(470, 22, 20, 472);
+        socket.emit("line", { x1: 470, y1: 22, x2: 20, y2: 472 }); 
+    }
+    canvas.onclick = false;
+}
   
 draw.drawX(160, 0, 160, 500, 330, 0, 330, 500);
 draw.drawX(0, 160, 500, 160, 0, 330, 500, 330);
@@ -67,33 +103,42 @@ draw.drawX(0, 160, 500, 160, 0, 330, 500, 330);
 function setSymbol(e){
   if (e.offsetY < 160 && e.offsetX < 160) {
     draw.drawSymbol(30, 30, 130, 130, 130, 30, 30, 130, 80, 80, 0);
+    sq0 = true;
   } 
   else if (e.offsetY < 160 && e.offsetX > 160 && e.offsetX < 330) {
     draw.drawSymbol(196, 30, 296, 130, 296, 30, 196, 130, 246, 80, 1);
+    sq1 = true;
   } 
   else if (e.offsetY < 160 && e.offsetX > 330) {
     draw.drawSymbol(362, 30, 462, 130, 462, 30, 362, 130, 412, 80, 2);
+    sq2 = true;
   } 
   else if (e.offsetY > 160 && e.offsetY < 330 && e.offsetX < 160) {
     draw.drawSymbol(30, 196, 130, 296, 130, 196, 30, 296, 80, 246, 3);
+    sq3 = true;
   } 
   else if (e.offsetY > 160 && e.offsetY < 330 && e.offsetX > 160 && e.offsetX < 330) {
     draw.drawSymbol(196, 196, 296, 296, 296, 196, 196, 296, 246, 246, 4);
+    sq4 = true;
   } 
   else if (e.offsetY > 160 && e.offsetY < 330 && e.offsetX > 330) {
     draw.drawSymbol(362, 196, 462, 296, 462, 196, 362, 296, 412, 246, 5);
+    sq5 = true;
   } 
   else if (e.offsetY > 330 && e.offsetX < 160) {
     draw.drawSymbol(30, 362, 130, 462, 130, 362, 30, 462, 80, 412, 6);
+    sq6 = true;
   } 
   else if (e.offsetY > 330 && e.offsetX > 160 && e.offsetX < 330) {
     draw.drawSymbol(196, 362, 296, 462, 296, 362, 196, 462, 246, 412, 7);
+    sq7 = true;
   } 
   else if (e.offsetY > 330 && e.offsetX > 330) {
     draw.drawSymbol(362, 362, 462, 462, 462, 362, 362, 462, 412, 412, 8);
+    sq8 = true;
   }
   counter++;
-
+  checkSquares();
 }
 
 canvas.onclick = function(e) { setSymbol(e); }
@@ -107,4 +152,7 @@ socket.on('goDraw', function(data) {
     canvas.onclick = function(e) { setSymbol(e); }
 });
 
-}())
+socket.on("sendLine", function(data) { 
+    draw.drawX(data.x1, data.y1, data.x2, data.y2); 
+    canvas.onclick = false;
+});
